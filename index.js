192fb7f4623ecf961b321d10a8d16f804fc125ea
Leslie <lesliesantana2209@yahoo.com>
2025-01-17 13:39:44 -0500

    started movie catalogue section

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
-const movies = await fetch("https://www.omdbapi.com/?i=tt3896198&apikey=1eb4f276&s") 
-console.log(movies)
+// async function main(){
+// const movies = await fetch(`https://www.omdbapi.com/?i=tt3896198&apikey=1eb4f276&s=${name}`) 
+// const movieTitle = await movies.json();
+// const movieListEl = document.querySelector('.movie')
+// movieListEl.innerHTML = movieTitle.map  
+
+// }
+// main();
+
+function movieHML(movie){
+
+}
