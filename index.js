59984e32411a24f775cc43ddeba71f13210c3d6e
Leslie <lesliesantana2209@yahoo.com>
2025-01-23 13:28:12 -0500

    search query

--- index.js
+++ index.js
@@ -7,6 +7,31 @@
 // }
 // main();
 
-function movieHML(movie){
+// function movieHML(movie){
+//  return `<div class="movie__card">
+//                 <h1 class="pretty">Title</h1>
+//                 <p class="pretty">Year</p>
+//              </div>
+//                 <div class="poster">
+//                     <img src="https://m.media-amazon.com/images/M/MV5BZGRiMDE1NTMtMThmZS00YjE4LWI1ODQtNjRkZGZlOTg2MGE1XkEyXkFqcGc@._V1_SX300.jpg" class= "img__poster"alt="">
+//                 </div>
+//             </div>`;
+// }
+document.addEventListener('DOMContentLoaded',() => {
+ const form = document.getElementById ('search__form')
+ form.addEventListener('submit', async (event) => {
+    event.preventDefault();
+    const query = document.querySelector('.input__search').value;
+    if (!query){
+        event.preventDefault();
+        alert('Please enter a movie title');
+        return; 
+    }
+ })
 
-}
+ });
+ 
+
+
+
+
